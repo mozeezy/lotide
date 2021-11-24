273649e1836814a1274dd5df46736b794fb3a566
Mohamed Mohamed <justmoe23@gmail.com>
2021-11-24 02:53:49 +0000

    compare 2 arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1);
 const eqArrays = function(arr1, arr2) {
   let correct = true;
 
