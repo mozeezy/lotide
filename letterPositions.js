de18f301ae944e8370b1c5d7571647455a09f135
Mohamed Mohamed <justmoe23@gmail.com>
2021-11-24 23:52:49 +0000

    implement letter positions

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const letterPositions = function(sentence) {
+// initialize an empty object
+  const results = {};
+// use a for loop: let iterating variable i = 0. for every iteration less than the length of the sentence array...  
+  for (let i = 0; i< sentence.length; i++) {
+  // if the sentence doesn't have space then...   
+    if (sentence[i] !== " ") {
+      // if i is not in results, then push it in.
+      results[sentence[i]] ? results[sentence[i]].push(i) : results[sentence[i]] =[i];
+    }
+  }
+  
+  return results;
+};
+
+const eqArrays = function(arr1, arr2) {
+  let correct = true;
+
+  
+  if (arr1.length !== arr2.length) {
+    return false
+  } 
+
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      correct = false;
+    }
+  }
+
+  return correct;
+
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays (arr1, arr2)) {
+    console.log (`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log (`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+};
+// -------- TEST ------------
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [1]);
