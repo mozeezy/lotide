b6c7f97e7b742342297793eafd911fa8098527bb
Mohamed Mohamed <justmoe23@gmail.com>
2021-11-25 00:30:06 +0000

    implement eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,30 @@
+/* const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+*/
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  let output = true;
+  const array1 = Object.keys(object1);
+  const array2 = Object.keys(object2);
+  if (array1.length !== array2.length) 
+    output = output && false;
+  else for (let key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+    output = output && (eqArrays(object1[key], object2[key]));
+    } else if (typeof(object1[key]) === "object" || typeof(object2[key]) === "object") {
+      output = output && eqObjects(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) output = output && false;
+  }
+  return output;
+};
+
