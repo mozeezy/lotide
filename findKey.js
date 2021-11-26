cac3d9ebc6a1568e55f02336e1143711b12684fd
Mohamed Mohamed <justmoe23@gmail.com>
2021-11-26 01:16:27 +0000

    implement findKey

---
+++ findKey.js
@@ -0,0 +1,51 @@
+const findKey = function(object, callback) {
+  // for.. in iterates through objects. let key be the iterating variable.
+  for (let key in object) {
+    // if 
+    if (callback(object[key])) 
+    return key;
+  }
+};
+
+// findKey function has 2 parameters: findKey(object, callback)
+//
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "Blue Hill");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "Ora");
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
