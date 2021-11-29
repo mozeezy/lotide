56a566c7578b3a0210362f53418f9bd0829dced0
Mohamed Mohamed <justmoe23@gmail.com>
2021-11-29 21:21:12 +0000

    export functions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,3 @@
-// const tail = require('../tail');
-// const assertEqual = require('../assertEqual');
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-
-// assertEqual(words.length, 3);
-
 const assert = require('chai').assert;
 const tail = require('../tail');
 
