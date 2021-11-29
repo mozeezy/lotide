0efae44812078466be8050f4931796b3d3ef7e8f
Mohamed Mohamed <justmoe23@gmail.com>
2021-11-29 19:56:04 +0000

    refactor tail function

--- tail.js
+++ tail.js
@@ -15,9 +15,7 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
 
 module.exports = tail;
 
