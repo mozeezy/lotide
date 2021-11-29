09ba1dae701f4abeb6ffd6982f5b45f5eed1a682
Mohamed Mohamed <justmoe23@gmail.com>
2021-11-29 19:59:23 +0000

    refactor eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -28,7 +28,6 @@ const eqArrays = function(arr1, arr2) {
   return correct;
 
 }
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true);
+
 
 module.exports = eqArrays;
